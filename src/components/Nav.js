a694148e6c61f65e0b56b7334029f6f59a64eb14
vivekkashyap <vivek.kashyap102@gmail.com>
2022-08-18 17:12:06 +0530

    Handling Logout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Nav = () => {
     const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    
+    const logout = () => {
+        localStorage.clear();
+        navigate('/signup');
+        console.warn("Logged Out...");
+    }
+
     return(
         <div>
             <ul className='nav-ul'>
                 <li><Link to='/'> Products </Link></li>
                 <li><Link to='/add'> Add Product </Link></li>
                 <li><Link to='/update'> Update Product </Link></li>
-                <li><Link to='/logout'> Logout </Link></li>
                 <li><Link to='/profile'> Profile </Link></li>
-                <li>{ auth ? <Link to='/logout'> Logout </Link> : <Link to='/signup'> Sign Up </Link> }</li>
+                <li>{ auth ? <Link onClick={logout} to='/signup'> Logout </Link> : <Link to='/signup'> Sign Up </Link> }</li>
             </ul>
         </div>
     )
