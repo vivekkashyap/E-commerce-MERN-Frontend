1fdd7f1ea262348447e237a251870b59d6a7ef90
vivekkashyap <vivek.kashyap102@gmail.com>
2022-08-18 18:31:01 +0530

    Fixed Navbar visibility

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,19 +13,20 @@ const Nav = () => {
 
     return(
         <div>
-            <ul className='nav-ul'>
-                <li><Link to='/'> Products </Link></li>
-                <li><Link to='/add'> Add Product </Link></li>
-                <li><Link to='/update'> Update Product </Link></li>
-                <li><Link to='/profile'> Profile </Link></li>
-                {
-                    auth ? <li><Link onClick={logout} to='/signup'> Logout </Link></li> :
-                        <>
-                            <li><Link to='/signup'> Sign Up </Link></li>
-                            <li><Link to='/login'> Login </Link></li>
-                        </>
-                }
-            </ul>
+            { auth ? 
+                <ul className='nav-ul'>
+                    <li><Link to='/'> Products </Link></li>
+                    <li><Link to='/add'> Add Product </Link></li>
+                    <li><Link to='/update'> Update Product </Link></li>
+                    <li><Link to='/profile'> Profile </Link></li>
+                    <li><Link onClick={logout} to='/signup'> Logout ({JSON.parse(auth).name}) </Link></li>
+                </ul>
+                :   
+                <ul className='nav-ul'>
+                    <li><Link to='/signup'> Sign Up </Link></li>
+                    <li><Link to='/login'> Login </Link></li>
+                </ul>
+            }
         </div>
     )
 }
