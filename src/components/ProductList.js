f673ff6e42e18b3c1bf744e84af340d0f55fc62b
vivekkashyap <vivek.kashyap102@gmail.com>
2022-08-19 13:49:16 +0530

    Delete API Integration

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { GET_PRODUCTS_URL, POST } from '../app.constant';
+import { DELETE, DELETE_PRODUCT_URL, GET_PRODUCTS_URL, POST } from '../app.constant';
 
 const ProductList = () => {
     const [products, setProducts] = useState([]);
@@ -14,6 +14,17 @@ const ProductList = () => {
         setProducts(result);
     }
 
+    const deleteProduct = async (id) => {
+        let result = await fetch(DELETE_PRODUCT_URL + id, {
+            method: DELETE
+        });
+        result = await result.json();
+        if(result) {
+            alert("Product Deleted Successfully");
+            getProducts();
+        }
+    }
+
     return (
         <div className='product-list'>
             <h3>Product List</h3>
@@ -23,15 +34,17 @@ const ProductList = () => {
                 <li> Price </li>
                 <li> Category </li>
                 <li> Company </li>
+                <li> Operation </li>
             </ul>
             {
                 products.map((item, index) =>
-                    <ul>
+                    <ul key={item._id}>
                     <li> {index + 1} </li>
                     <li> {item.name} </li>
                     <li> ₹{item.price} </li>
                     <li> {item.category} </li>
                     <li> {item.company} </li>
+                    <li> <button onClick={() => deleteProduct(item._id)}>Delete</button> </li>
                 </ul>
                 )
             }
